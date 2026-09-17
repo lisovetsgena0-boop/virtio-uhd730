649dd265fc2685174e250cb26e3c18afa8b4c843
lisovetsgena0-boop <lisovetsgena0@gmail.com>
2026-09-17 23:49:56 +0300

    Persist game settings and expanded save state

--- echo-rivne-src/scripts/SaveSystem.gd
+++ echo-rivne-src/scripts/SaveSystem.gd
@@ -2,18 +2,40 @@ extends Reference
 class_name SaveSystem
 
 const PATH = "user://echo_rivne_save.json"
+const SETTINGS_PATH = "user://echo_rivne_settings.json"
 
 static func save(data):
     var f = File.new()
-    if f.open(PATH, File.WRITE) != OK: return false
+    if f.open(PATH, File.WRITE) != OK:
+        return false
     f.store_string(to_json(data))
     f.close()
     return true
 
 static func load():
     var f = File.new()
-    if not f.file_exists(PATH): return null
-    if f.open(PATH, File.READ) != OK: return null
+    if not f.file_exists(PATH):
+        return null
+    if f.open(PATH, File.READ) != OK:
+        return null
+    var txt = f.get_as_text()
+    f.close()
+    return parse_json(txt)
+
+static func save_settings(data):
+    var f = File.new()
+    if f.open(SETTINGS_PATH, File.WRITE) != OK:
+        return false
+    f.store_string(to_json(data))
+    f.close()
+    return true
+
+static func load_settings():
+    var f = File.new()
+    if not f.file_exists(SETTINGS_PATH):
+        return null
+    if f.open(SETTINGS_PATH, File.READ) != OK:
+        return null
     var txt = f.get_as_text()
     f.close()
     return parse_json(txt)
